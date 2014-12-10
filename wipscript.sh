6c81004c0af73a4c663af8f51dcb92de9e1d3e5c
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-12-10 12:09:15 +0000

    new script

---
+++ wipscript.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# usege: wipscript.sh repo file branch
+# for example
+# wipscript.sh ~/repo file.sh development
+
+# echo "Starting"
+repo=$1
+file=$2
+branch=$3
+
+cd $repo
+hv=`git rev-parse HEAD`
+
+git add $file
+git commit $file -m $file
+
+git checkout wip/$branch
+git checkout $branch -- $file
+git commit -am $file
+
+git checkout $branch
+git reset --soft $hv
+
+# echo "Finished."
